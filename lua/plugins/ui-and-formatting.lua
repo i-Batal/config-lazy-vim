683c2bd83959795f37302df0c899a8be8423eda6
ilyessb-padoa <ilyess.b.ext@padoa-group.com>
2026-04-17 16:57:15 +0200

    add

--- lua/plugins/ui-and-formatting.lua
+++ lua/plugins/ui-and-formatting.lua
@@ -18,6 +18,23 @@ return {
 
   {
     "stevearc/conform.nvim",
+    init = function()
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = { "sql", "mysql", "plsql" },
+        callback = function()
+          vim.b.autoformat = false
+        end,
+      })
+
+      vim.api.nvim_create_user_command("Format", function()
+        require("conform").format({
+          async = false,
+          lsp_format = "never",
+        })
+      end, { desc = "Format buffer manually" })
+
+      vim.cmd([[cnoreabbrev <expr> format getcmdtype() == ':' && getcmdline() == 'format' ? 'Format' : 'format']])
+    end,
     opts = function(_, opts)
       opts.formatters_by_ft = opts.formatters_by_ft or {}
       opts.formatters_by_ft.sql = { "pg_format" }
@@ -25,8 +42,10 @@ return {
       opts.formatters_by_ft.plsql = { "pg_format" }
 
       opts.formatters = opts.formatters or {}
+      local pg_format = vim.fn.exepath("pg_format")
       opts.formatters.pg_format = {
-        args = { "-k", "2", "-s", "2", "-w", "120", "-" },
+        command = pg_format ~= "" and pg_format or "/opt/homebrew/bin/pg_format",
+        args = { "-u", "2", "-s", "2", "-w", "120", "-" },
       }
     end,
   },
